dc5720ce63ab9b88fdbe35fb9e5fa58a49e951b5
moonyeju <moonyeju8510@gmail.com>
2023-09-25 18:02:54 +0900

    [ADD] count num main page

--- kogas_app/src/screens/HomeScreen.js
+++ kogas_app/src/screens/HomeScreen.js
@@ -3,20 +3,73 @@ import { BLACK, GRAY } from '../color';
 import {useNavigation} from '@react-navigation/native';
 import { FlatList } from 'react-native-gesture-handler';
 import ListItem from '../components/ListItem';
+import { useEffect, useState } from 'react';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const mydocs = {
-    all: 3,
-    clear: 2,
-  }
+      id: 1,
+      status: '서명진행중', //상태
+      title: '모바일 구축사업 인수인계 협의2',
+      department: '수요공급처',
+      name: 'aa',
+      registrationDate: '2023-09-25', //등록 날짜
+      content: [{
+        department: '계약운용부',
+        name: 'aaa',
+        status: '서명완료',
+        date: '2023/09/25 15:00'
+      }, {
+        department: '발전영업부',
+        name: 'aaaa',
+        status: '서명 진행중',
+        date: '2023/09/25 15:00'
+        }]
+    }
   const newdocs = {
-    all: 4,
-    clear: 2,
+      id: 1,
+      status: '서명진행중', //상태
+      title: '모바일 구축사업 인수인계 협의2',
+      department: '수요공급처',
+      name: 'bb',
+      registrationDate: '2023-09-25', //등록 날짜
+      content: [{
+        department: '계약운용부',
+        name: 'bbb',
+        status: '서명완료',
+        date: '2023/09/25 15:00'
+      }, {
+        department: '계약운용부',
+        name: 'bbb',
+        status: '서명 진행중',
+        date: '2023/09/25 15:00'
+      },{
+        department: '발전영업부',
+        name: 'bbbb',
+        status: '서명완료',
+        date: '2023/09/25 15:00'
+        }]
   }
-  const myperc = Math.floor(mydocs.clear / mydocs.all * 100);
-  const newperc = Math.floor(mydocs.clear / mydocs.all * 100);
+  const [mycount, setMyCount] = useState(0); // useState로 상태 변수 관리
+  const [newcount, setNewCount] = useState(0); // useState로 상태 변수 관리
 
+  const myperc = Math.floor((mycount / mydocs.content.length) * 100); // mycount 사용
+  const newperc = Math.floor((newcount / newdocs.content.length) * 100); // newcount 사용
+
+  useEffect(() => {
+    mydocs.content.forEach((item) => {
+      if (item.status === '서명완료') {
+        setMyCount((prevCount) => prevCount + 1); // 상태 업데이트
+      }
+    });
+//중복되서 커짐 해결필요
+    newdocs.content.forEach((item) => {
+      if (item.status === '서명완료') {
+        setNewCount((prevCount) => prevCount + 1); // 상태 업데이트
+      }
+    });
+  }, []);
+  
   const List = [
     {
       id: 1,
@@ -54,26 +107,26 @@ const HomeScreen = () => {
       <Text>내 진행 상황</Text>
       <Pressable
             onPress={() => {
-              navigation.navigate('SignatureList');
+              navigation.navigate('Signature');
             }}
         hitSlop={10}
         style={styles.mydocs}
       >
         <View>
           <Text>{myperc}%</Text><Text>내 문서 진행률</Text>
-          <Text>서명 대상자 {mydocs.all}명 중 {mydocs.clear}명이 서명을 완료하였습니다.</Text>
+          <Text>서명 대상자 {mydocs.content.length}명 중 {mycount}명이 서명을 완료하였습니다.</Text>
         </View>
       </Pressable>
       <Pressable
-            // onPress={() => {
-            //   navigation.navigate('SignatureList');
-            // }}
+            onPress={() => {
+              navigation.navigate('ListDetail');
+            }}
         hitSlop={10}
         style={styles.mydocs}
       >
         <View>
           <Text>{newperc}%</Text><Text>최근 문서 진행률</Text>
-          <Text>서명 대상자 {newdocs.all}명 중 {newdocs.clear}명이 서명을 완료하였습니다.</Text>
+          <Text>서명 대상자 {newdocs.content.length}명 중 {newcount}명이 서명을 완료하였습니다.</Text>
         </View>
       </Pressable>
     </View>
